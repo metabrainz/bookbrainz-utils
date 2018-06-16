5529379cfed712a33011d2e88b5e8b2349b07309
Shivam Tripathi <shivamtripathi0108@gmail.com>
2018-06-16 11:26:41 +0530

    feat(consumers/validators): Add validation code taken from bb-site

---
+++ src/consumer/validators/types.js
@@ -0,0 +1,42 @@
+/*
+ * Taken from bookbrainz-site.
+ * Copyright (C) 2017  Ben Ockmore
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+// @flow
+
+export type _Creator = {
+
+};
+
+export type _CreatorType = {
+
+};
+
+export type _Gender = {
+
+};
+
+export type _IdentifierType = {
+	id: number,
+	label: string,
+	validationRegex: string
+};
+
+export type _Language = {
+
+};
