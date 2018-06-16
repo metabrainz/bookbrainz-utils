105eddd1f984ba037fbc5aede497a15928fc41e2
Shivam Tripathi <shivamtripathi0108@gmail.com>
2018-06-16 10:01:46 +0530

    feat(helpers/utils): Add functions to get sorted name of a name string

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2018  Shivam Tripathi
+ * Copyright (C) 2018 Ben Ockmore
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,6 +17,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+// @flow
 
 import _ from 'lodash';
 
@@ -65,4 +67,77 @@ export function splitArray(array, parts) {
 	const returnArray = _.chunk(array, splitSize);
 
 	return returnArray;
+}
+
+/**
+ * stripDot: Taken from bookbrainz-site
+ * Removes all period characters (dots) from the input string, returning a new
+ * string.
+ *
+ * @param {String} name the input string to strip
+ * @returns {String} the string with dots removed
+ */
+function stripDot(name: string): string {
+	return name.replace(/\./g, '');
+}
+
+/**
+ * sortName: Taken from bookbrainz-site
+ * Returns sorted string of the input name string
+ * @param {string} name - name whose sorted version is to be found
+ * @returns {string} - sortedName which is sorted version of the input
+ */
+export function sortName(name: string): string {
+	const articles = ['a', 'an', 'the', 'los', 'las', 'el', 'la'];
+	const suffixes = [
+		'i', 'ii', 'iii', 'iv', 'v', 'vi', 'vii', 'viii', 'ix', 'x', 'xi',
+		'xii', 'xiii', 'xiv', 'xv', 'jr', 'junior', 'sr', 'senior', 'phd', 'md',
+		'dmd', 'dds', 'esq'
+	];
+
+	/*
+	 * Remove leading and trailing spaces, and return a blank sort name if
+	 * the string is empty
+	 */
+	const trimmedName = name.trim();
+	if (trimmedName.length === 0) {
+		return '';
+	}
+
+	const words = trimmedName.replace(/,/g, '').split(' ');
+
+	// If there's only one word, simply copy the name as the sort name
+	if (words.length === 1) {
+		return trimmedName;
+	}
+
+	// First, check if sort name is for collective, by detecting article
+	const firstWord = stripDot(words[0]);
+	const firstWordIsArticle = articles.includes(firstWord.toLowerCase());
+	if (firstWordIsArticle) {
+		// The Collection of Stories --> Collection of Stories, The
+		return `${words.slice(1).join(' ')}, ${firstWord}`;
+	}
+
+	/*
+	 * From here on, it is assumed that the sort name is for a person
+	 * Split suffixes
+	 */
+	const isWordSuffix =
+		words.map((word) => suffixes.includes(stripDot(word).toLowerCase()));
+	const lastSuffix = isWordSuffix.lastIndexOf(false) + 1;
+
+	// Test this to check that splice will not have a 0 deleteCount
+	const suffixWords =
+		lastSuffix < words.length ? words.splice(lastSuffix) : [];
+
+	// Rearrange names to (last name, other names)
+	const INDEX_BEFORE_END = -1;
+
+	let [lastName] = words.splice(INDEX_BEFORE_END);
+	if (suffixWords.length > 0) {
+		lastName += ` ${suffixWords.join(' ')}`;
+	}
+
+	return `${lastName}, ${words.join(' ')}`;
 }
