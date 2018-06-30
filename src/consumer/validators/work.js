5ed01bfe156a041f7dbfb77c2f860474e407f319
Shivam Tripathi <shivamtripathi0108@gmail.com>
2018-06-30 22:13:52 +0530

    refactor(consumers/validators/work): Refactor code, add logging

--- src/consumer/validators/work.js
+++ src/consumer/validators/work.js
@@ -1,5 +1,5 @@
 /*
- * Taken from bookbrainz-site.
+ * Adapted from bookbrainz-site.
  * Copyright (C) 2017  Ben Ockmore
  *
  * This program is free software; you can redistribute it and/or modify
@@ -22,11 +22,11 @@
 
 import {get, validatePositiveInteger} from './base';
 import {
-	validateAliases, validateIdentifiers, validateNameSection,
-	validateSubmissionSection
+	validateAliases, validateIdentifiers, validateNameSection
 } from './common';
 import _ from 'lodash';
 import type {_IdentifierType} from './types';
+import log from '../../helpers/logger';
 
 
 export function validateWorkSectionType(value: ?any): boolean {
@@ -49,17 +49,52 @@ export function validateWorkSection(data: any): boolean {
 }
 
 export function validateWork(
-	formData: any, identifierTypes?: ?Array<_IdentifierType>
+	validationObject: any,
+	identifierTypes?: ?Array<_IdentifierType>
 ): boolean {
-	const conditions = [
-		validateAliases(get(formData, 'aliasSection', {})),
-		validateIdentifiers(
-			get(formData, 'identifierSection', {}), identifierTypes
-		),
-		validateNameSection(get(formData, 'nameSection', {})),
-		validateWorkSection(get(formData, 'workSection', {})),
-		validateSubmissionSection(get(formData, 'submissionSection', {}))
-	];
+	let success = true;
 
-	return _.every(conditions);
+	const {workerId, ...workValidationObject} = validationObject;
+	if (_.isEmpty(workValidationObject)) {
+		log.warning(`[CONSUMER::${workerId}] WORK Incoming validation object \
+			\rempty`);
+		return false;
+	}
+
+	// Cumulative error messages to be stored in err string
+	let err = '';
+	const aliasSection = get(workValidationObject, 'aliasSection', {});
+	const identifierSection = get(
+		workValidationObject, 'identifierSection', {}
+	);
+	const nameSection = get(workValidationObject, 'nameSection', {});
+	const workSection = get(workValidationObject, 'workSection', {});
+
+	log.info(`[CONSUMER::${workerId}] WORK - Calling validation functions.`);
+
+	if (!validateAliases(aliasSection)) {
+		err += 'WORK - Failed validate alias section failed. \n';
+		success = false;
+	}
+
+	if (!validateIdentifiers(identifierSection, identifierTypes)) {
+		err += 'WORK - Validate identifier section failed. \n';
+		success = false;
+	}
+
+	if (!validateNameSection(nameSection)) {
+		err += 'WORK - Validate name section failed. \n';
+		success = false;
+	}
+
+	if (!validateWorkSection(workSection)) {
+		err += 'WORK - Validate work section failed. \n';
+		success = false;
+	}
+
+	if (!success) {
+		log.error(`[CONSUMER::${workerId}]:: ${err} Record for reference:
+			\r${JSON.stringify(validationObject, null, 4)}`);
+	}
+	return success;
 }
