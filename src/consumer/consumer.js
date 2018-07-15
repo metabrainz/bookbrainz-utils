048d18570304e96086135e2f1b70a5b8f183ab0f
Shivam Tripathi <shivamtripathi0108@gmail.com>
2018-07-15 19:17:20 +0530

    feat(consumer/consumer): Add db support and retry code

--- src/consumer/consumer.js
+++ src/consumer/consumer.js
@@ -18,10 +18,13 @@
 
 
 import * as Error from '../helpers/errors';
+import BookBrainzData from 'bookbrainz-data';
 import Promise from 'bluebird';
 import {Queue} from '../queue';
+import _ from 'lodash';
+import async from 'async';
+import config from '../helpers/config';
 import consumeRecord from './consumeRecord';
-import {isNotDefined} from '../helpers/utils';
 import log from '../helpers/logger';
 
 
@@ -36,6 +39,10 @@ import log from '../helpers/logger';
 function consumerPromise({id, init}) {
 	log.notice(`[WORKER::${id}] Running consumer number ${id}`);
 
+	const orm = BookBrainzData(config('database'));
+	const importRecord = _.partial(orm.func.imports.createImport, orm);
+	const {retryLimit} = config('import');
+
 	// A never resolving promise as consumer is supposed to run forever
 	return new Promise(() => {
 		const queue = new Queue(init);
@@ -44,43 +51,105 @@ function consumerPromise({id, init}) {
 			Error.undefinedValue('Consumer instance:: Worker Id undefined');
 		}
 
-		async function messageHandler(msg) {
+		function messageHandler(msg) {
 			log.notice(`[CONSUMER::${id}] Received object.\
 				\r Running message handler`);
+
 			if (typeof msg === 'undefined' || !msg) {
 				log.error('Empty Message received. Skipping.');
 				return;
 			}
 
+			// Attempts left for this message
+			let attemptsLeft = retryLimit;
 			const record = JSON.parse(msg.content.toString());
-			const error = await consumeRecord({workerId: id, ...record});
 
-			switch (error) {
-				case Error.NONE:
-					log.info(
-						`[CONSUMER::${id}] Read message successfully
-						\r${record}`
-					);
-					queue.acknowledge(msg);
-					break;
-				case Error.INVALID_RECORD:
-				case Error.RECORD_ENTITY_NOT_FOUND:
-					log.warning(
-						`[CONSUMER::${id}] ${error} - \
-						\r Skipping the errored record.`
-					);
-					queue.acknowledge(msg);
-					break;
-				case Error.TRANSACTION_ERROR:
-					log.warning(
-						`[CONSUMER::${id}] ${error} Setting up for reinsertion.
-						\r Record for reference:: \n ${record}`
-					);
-					break;
-				default: break;
-			}
+			// Manages consume record retries
+			async.whilst(
+				// Test whose return value if is false, stops the iteration
+				// When number of attempts finish up, the test should return F
+				() => attemptsLeft > 0,
+
+				// Function repeated upon transaction error for retries times
+				// Manages async record consumption
+				async (cb) => {
+					if (attemptsLeft < retryLimit) {
+						log.info(`
+						\r ##################################################
+						\r Restarting import process....
+						\r ##################################################
+						`);
+					}
+
+					log.info('Running async function');
+					const error = await consumeRecord({
+						importRecord,
+						workerId: id,
+						...record
+					});
+
+					switch (error) {
+						case Error.NONE:
+							// On success, we don't need to retry again
+							attemptsLeft = 0;
+							log.info(
+								`[CONSUMER::${id}] Read message successfully
+								\r${record}`
+							);
+							queue.acknowledge(msg);
+							break;
+
+						case Error.INVALID_RECORD:
+						case Error.RECORD_ENTITY_NOT_FOUND:
+							// In case of invalid records, we don't try again
+							attemptsLeft = 0;
+							log.warning(
+								`[CONSUMER::${id}] ${error} -\
+								\r Skipping the errored record.`
+							);
+							// As we're not retrying, we acknowledge the message
+							queue.acknowledge(msg);
+							break;
+
+						case Error.TRANSACTION_ERROR:
+							// In case of transaction errors, we retry a number
+							// 		of times before giving up
+							attemptsLeft--;
+
+							// Issue a warning in case of transaction error
+							log.warning(
+								`[CONSUMER::${id}] ${error} Setting up for\
+								\r reinsertion. Record for reference:
+								\r ${msg}
+								\r Attempts left: ${attemptsLeft}`
+							);
+
+							// If no more attempts left, acknowledge the message
+							if (!attemptsLeft) {
+								log.info('No more attempts left.',
+									'Acknowledging the message.');
+								queue.acknowledge(msg);
+							}
+
+							log.info(attemptsLeft);
+							break;
+
+						default: {
+							throw new Error(
+								'Undefined response while importing'
+							);
+						}
+					}
+
+					// In case of no error, return back the present count of
+					//		attemptsLeft
+					cb(null, attemptsLeft);
+				},
+
+				// Raise error in case of error
+				Error.raiseError(Error.IMPORT_ERROR)
+			);
 		}
-
 		// Connection related errors would be handled on the queue side
 		return queue.consume(messageHandler);
 	});
