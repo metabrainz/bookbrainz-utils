74bee3f8a217a9dfb676562da4d4048f2a24e476
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-07-25 15:54:15 +0200

    chore: Enhance author testdata with aliases, identifiers and areas

--- importer/testdata/author.ts
+++ importer/testdata/author.ts
@@ -8,15 +8,33 @@ export const entity: QueuedEntity<ParsedAuthor> = {
 			"languageId": 120, // English, TODO: use language codes
 			"default": true,
 			"primary": true
+		}, {
+			"name": "MusicBrainz Test Artist",
+			"sortName": "Author, Test",
+			"languageId": 120, // English
+			"primary": false
+		}, {
+			"name": "Auteur du Test",
+			"sortName": "Test, Auteur du",
+			"languageId": 134, // French
+			"primary": true
 		}],
 		"disambiguation": "import test",
 		"type": "Person", // unused, TODO: map to ID
 		"typeId": 1, // Person
 		"genderId": 3, // Other
+		"beginAreaId": 8558, // Hill
 		"beginDate": "1701-04-23",
+		"endAreaId": 98703, // Ciudad Nezahualcóyotl
 		"endDate": "1742-12-31",
 		"ended": true,
-		"identifiers": [],
+		"identifiers": [{
+			"typeId": 2, // MusicBrainz Artist ID
+			"value": "7e84f845-ac16-41fe-9ff8-df12eb32af55"
+		}, {
+			"typeId": 23, // OpenLibrary Author ID
+			"value": "OL3673719A"
+		}],
 		"annotation": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto enim reprehenderit dolorum iure eaque accusamus, soluta pariatur aliquam commodi repellat cumque quaerat possimus nesciunt molestiae. Sunt deserunt magnam sint repellendus.",
 		"metadata": { // currently unused
 			"links": [{
@@ -28,7 +46,7 @@ export const entity: QueuedEntity<ParsedAuthor> = {
 		"source": "Testdata"
 	},
 	"entityType": "Author",
-	"lastEdited": "2024-07-17",
-	"originId": "A123", // change this to import another dummy entity
+	"lastEdited": "2024-07-25",
+	"originId": "A1234", // change this to import another dummy entity
 	"source": "Testdata"
 };
